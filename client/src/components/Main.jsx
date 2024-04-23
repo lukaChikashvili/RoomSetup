b1d611cf0db148b8199223ac776549c62dcc6a62
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-23 17:34:05 +0400

    add table position settings

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {  Html, OrbitControls, useGLTF } from '@react-three/drei'
 import { useLoader } from '@react-three/fiber'
-import { TextureLoader } from 'three'
+import { MeshStandardMaterial, TextureLoader } from 'three'
 
 const Main = ({textureSource, modelSource}) => {
   
@@ -11,10 +11,11 @@ const Main = ({textureSource, modelSource}) => {
 
     const [setting, setSetting] = useState(false);
     const [size, setSize] = useState(1);
-
+    const [xPosition, setXPosition] = useState(0);
+    const [yPosition, setYPosition] = useState(0);
 
     const handleMesh = () => {
-      setSetting(!setting);
+      setSetting(true);
     }
 
     const handleSize = (event) => {
@@ -24,7 +25,27 @@ const Main = ({textureSource, modelSource}) => {
       }
   }
 
+  const handlePosition = (event) => {
+    const newSize = parseFloat(event.target.value);
+      if (!isNaN(newSize)) {
+          setXPosition(newSize);
+      }
 
+      if(newSize === 5 || newSize === -5) {
+        setXPosition(0);
+      }
+  }
+
+  const handleYPosition = (event) => {
+    const newSize = parseFloat(event.target.value);
+    if (!isNaN(newSize)) {
+        setYPosition(newSize);
+    }
+
+    if(newSize === 5 || newSize === -5) {
+      setYPosition(0);
+    }
+  }
 
 
   return (
@@ -41,13 +62,31 @@ const Main = ({textureSource, modelSource}) => {
         
         
        
-        <primitive object={model.scene} position-y = {-1} onClick = {handleMesh} scale = {size} />
+        <primitive object={model.scene} 
+                   position-y = {-1}
+                   onClick = {handleMesh}
+                   scale = {size} 
+                   position-x = {xPosition}
+                   position-z = {-yPosition}
+                    />
+
         {setting && <Html position={-2}>
              <div className='settings'>
                 <p>პარამეტრები</p>
+                <span onClick={() => setSetting(false)}>X</span>
                 <div className='label'>
                  <label>ზომა</label>
                  <input type='number' value = {size} onChange={handleSize}/>
+                 </div>
+
+                 <div className='label'>
+                 <label>პოზიცია X: </label>
+                 <input type='number' value = {xPosition} onChange={handlePosition}/>
+                 </div>
+
+                 <div className='label'>
+                 <label>პოზიცია Z: </label>
+                 <input type='number' value = {yPosition} onChange={handleYPosition}/>
                  </div>
              </div>
           </Html>}
